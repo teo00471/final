a3996b0e7046d824fdf86d60ce42a75099ceebf5
teo00471 <94004199+teo00471@users.noreply.github.com>
2023-12-02 05:12:44 -0500

    Update boot.py

--- boot.py
+++ boot.py
@@ -1,4 +1,59 @@
+from connection import do_connect
+#from ufirebase import firebase_real_time_database
+from ufirebase import firebase_get_data
+#from post_data import init
+#from post_to_dynamo import do_post
+from temperature import get_temperature
+import time
+import gc
+import machine
+from firestorage import bratzinai
+import uart
+#from firestorage import post_data
 import ugit
 
-
 ugit.pull_all()
+
+mainfunction()
+
+
+def mainfunction():
+    gc.collect()
+    do_connect()
+    
+    fan = machine.Pin(2,machine.Pin.OUT)
+    light = machine.Pin(16,machine.Pin.OUT)
+    while True:
+        gc.collect()
+        temp = get_temperature()
+        if temp > 30:
+            fan.on()
+        else:
+            fan.off()
+        data_from_firebase = firebase_get_data('ring/ring')
+        data_from_firebase_two = firebase_get_data('ring_time/ring_time')
+        print('data from firebase ->', data_from_firebase)
+        if data_from_firebase == True:
+            print('fan on')
+            fan.on()
+        else:
+            fan.off()
+        if data_from_firebase_two == True:
+            ligth.on()
+            print('light on')
+        else :
+            light.off()
+        print('data from firebase two ->', data_from_firebase_two)
+        try:
+            print('temperature successs ==>', temp)
+            bratzinai('chameleon-test', temp)
+        except Exception as e:
+            print('Can not post', e)
+        data_from_firebase_uart = firebase_get_data('uart/uart')
+        print('uart data', data_from_firebase_uart)
+        if data_from_firebase_uart:
+            uart.main()
+            time.sleep(4)
+        time.sleep(1)
+        
+        gc.collect()
